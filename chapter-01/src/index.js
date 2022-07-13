de88b0ea43fdd630ef9e4d1782eb64c59bfe4f48
Robinson Enedino <robinsonbrz@gmail.com>
2022-07-13 16:39:13 -0300

    Render React

--- chapter-01/src/index.js
+++ chapter-01/src/index.js
@@ -1,1 +1,11 @@
-console.log('Rick and Morty');
+import ReactDOM from 'react-dom/client';
+
+function App() {
+    return <h1>Rick and Morty</h1>;
+}
+
+const container = document.getElementById('app');
+const root = ReactDOM.createRoot(container);
+
+
+root.render(<App />);
